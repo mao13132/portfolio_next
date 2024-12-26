08f04caf438d7c8c471bec79af37f3c57ca345b5
mao13132 <mao13132@gmail.com>
2024-12-26 10:42:56 +0300

    fix css portfolio-works wrappers

--- app/Components/PortfolioWrapper/ProjectsPortfolio/ProjectsPortfolio.tsx
+++ app/Components/PortfolioWrapper/ProjectsPortfolio/ProjectsPortfolio.tsx
@@ -9,25 +9,23 @@ import Link from "next/link";
 export const ProjectsPortfolio = ({ icon, image, link, text, title, className, ...props }: ProjectsPortfolioProps): JSX.Element => {
     return (
         <div className={cn(className, styles["box"])} {...props}>
+            <Link href={link} className={styles['wrapp']}>
+                <Image src={`${process.env.NEXT_PUBLIC_FRONTEND}/${image}`}
+                    alt=""
+                    width={100}
+                    height={100}
+                    style={{ objectFit: 'cover' }}
+                    sizes="100" />
 
-            <Image src={`${process.env.NEXT_PUBLIC_FRONTEND}/${image}`}
-                alt=""
-                width={100}
-                height={100}
-                style={{ objectFit: 'cover' }}
-                sizes="100" />
+                <div className={styles['layer']}>
+                        <h4>{title}</h4>
 
-            <div className={styles['layer']}>
-                <Link href={link} className={styles['desc']}>
-                    <h4>{title}</h4>
+                        {/* <p>{text}</p>
 
-                    <p>{text}</p>
+                        <i className={icon} ></i> */}
 
-                    <i className={icon} ></i>
-                </Link>
-
-            </div>
-
+                </div>
+            </Link>
         </div>
     );
 };
